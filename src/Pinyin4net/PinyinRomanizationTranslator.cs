b25ad96035c1a9034bae460ae8f906b333efdfb3
jianyuelin <jianyuelin@westwin.com>
2020-12-22 17:54:27 +0800

    add Wade support

---
+++ src/Pinyin4net/PinyinRomanizationTranslator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Pinyin4net
+{
+    public class PinyinRomanizationTranslator
+    {
+        public static String ConvertRomanizationSystem(
+            String sourcePinyinStr,
+            PinyinRomanizationType sourcePinyinSystem,
+            PinyinRomanizationType targetPinyinSystem)
+        {
+            String pinyinString = TextHelper.ExtractPinyinString(sourcePinyinStr);
+            String toneNumberStr = TextHelper.ExtractToneNumber(sourcePinyinStr);
+
+            // return value
+            String targetPinyinStr = null;
+            // find the node of source Pinyin system
+            String xpathQuery1 =
+                "//" + sourcePinyinSystem.GetDescription<PinyinRomanizationType>() + "[text()='" +
+                pinyinString + "']";
+
+
+            var pinyinMappingDoc = XDocument.Load(
+                Assembly.GetExecutingAssembly().GetManifestResourceStream(
+                    "Pinyin4net.Resources.pinyin_mapping.xml"));
+            var nav = pinyinMappingDoc.CreateNavigator();
+            var hanyuNode = nav.SelectSingleNode(xpathQuery1);
+
+            if (null != hanyuNode)
+            {
+                // find the node of target Pinyin system
+                String xpathQuery2 = "../" + targetPinyinSystem.GetDescription<PinyinRomanizationType>() +
+                                     "/text()";
+                String targetPinyinStrWithoutToneNumber = hanyuNode.SelectSingleNode(xpathQuery2).Value;
+
+                targetPinyinStr = targetPinyinStrWithoutToneNumber + toneNumberStr;
+            }
+
+            return targetPinyinStr;
+        }
+    }
+}
---
+++ src/Pinyin4net/TextHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pinyin4net
+{
+    public static class TextHelper
+    {
+        /**
+       * @param hanyuPinyinWithToneNumber
+       * @return Hanyu Pinyin string without tone number
+       */
+        public static String ExtractToneNumber(String hanyuPinyinWithToneNumber)
+        {
+            return hanyuPinyinWithToneNumber.Substring(hanyuPinyinWithToneNumber.Length - 1);
+        }
+
+        /**
+       * @param hanyuPinyinWithToneNumber
+       * @return only tone number
+       */
+        public static String ExtractPinyinString(String hanyuPinyinWithToneNumber)
+        {
+            return hanyuPinyinWithToneNumber.Substring(0, hanyuPinyinWithToneNumber.Length - 1);
+        }
+    }
+}
